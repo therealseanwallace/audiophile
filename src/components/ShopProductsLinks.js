79d07390727a165ec6e718f8645952fb4bab46e4
Sean Wallace <wallace.sean111@gmail.com>
2023-01-09 12:05:03 +0100

    Add further styles to homepage

--- src/components/ShopProductsLinks.js
+++ src/components/ShopProductsLinks.js
@@ -1,20 +1,23 @@
 import React from "react";
+import HeadphonePreview from "../assets/headphone-preview.png";
+import SpeakerPreview from "../assets/speaker-preview.png";
+import EarphonePreview from "../assets/earphone-preview.png";
 
 const ShopProductsLinks = () => {
   return (
     <div className="shop-products-links-container">
       <button>
-        <img />
+        <img src={HeadphonePreview} className="headphone-preview preview-image"/>
         <h3>Headphones</h3>
         <p>Shop</p>
       </button>
       <button>
-        <img />
+      <img src={SpeakerPreview} className="speaker-preview preview-image"/>
         <h3>Speakers</h3>
         <p>Shop</p>
       </button>
       <button>
-        <img />
+        <img src={EarphonePreview} className="earphone-preview preview-image"/>
         <h3>Earphones</h3>
         <p>Shop</p>
       </button>
